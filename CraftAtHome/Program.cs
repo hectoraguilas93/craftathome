991ba14a934945be50f066bd4e2f1a4ef1cc9dd5
Carlos Gallegos <40046747+CharlyZest@users.noreply.github.com>
2021-09-04 02:43:15 -0500

    Fix heroku port

--- CraftAtHome/Program.cs
+++ CraftAtHome/Program.cs
@@ -20,6 +20,8 @@ namespace CraftAtHome
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.ConfigureKestrel(o =>
+                        o.Listen(System.Net.IPAddress.Any, Convert.ToInt32(Environment.GetEnvironmentVariable("PORT"))));
                     webBuilder.UseStartup<Startup>();
                 });
     }
--- CraftAtHome/Startup.cs
+++ CraftAtHome/Startup.cs
@@ -46,9 +46,10 @@ namespace CraftAtHome
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CraftAtHome v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CraftAtHome v1"));
 
             app.UseHttpsRedirection();
 
